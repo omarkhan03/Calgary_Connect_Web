6bde82c1b7336094c10a253b74785bd5311caf6f
Xiaomeng Li <641380@ACDM.DS.SAIT.CA>
2022-07-24 10:05:27 -0600

    Map and event function

--- server/src/app.js
+++ server/src/app.js
@@ -6,31 +6,28 @@ const Location = require('../models/locations');
 const formatedData = require("./data")
 
 
-mongoose.connect('mongodb://localhost:27017/yycconnects')
-    .then(() => {
-        console.log('Connection Open!!!');
-    })
-    .catch(() => {
-        console.log('Error!!!');
-    })
+// Database connection
+// mongoose.connect('mongodb://localhost:27017/yycconnects')
+//     .then(() => {
+//         console.log('Connection Open!!!');
+//     })
+//     .catch(() => {
+//         console.log('Error!!!');
+//     })
 
-
-
-const db = mongoose.connection;
-
+// const db = mongoose.connection;
 
 const app = express();
 
-
+// Setting up view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-// app.get('/', (req,res)=>{
-    // res.render('home')
-// })
-
+// Serve Static files
 app.use('/', express.static(path.join(__dirname, '../static')))
-
+app.use('/events', express.static(path.join(__dirname, '../static/events.html')))
+app.use('/food', express.static(path.join(__dirname, '../static/food.html')))
+app.use('/mappage', express.static(path.join(__dirname, '../static/mappage.html')))
 
 app.get('/sites', async (req, res) => {
     const sites = await Location.find({});
@@ -68,34 +65,11 @@ app.delete('/sites/:id', async (req, res) => {
     res.redirect('/sites');
 })
 
+// Server JSON data
 app.get("/data", (req, res) => {
     res.send(formatedData)
 })
 
-app.use('/events', express.static(path.join(__dirname, '../static/events.html')))
-
-app.use('/food', express.static(path.join(__dirname, '../static/food.html')))
-
-app.use('/mappage', express.static(path.join(__dirname, '../static/mappage.html')))
-
-
-const axios = require("axios");
-
-const options = {
-  method: 'GET',
-  url: 'https://google-maps-geocoding.p.rapidapi.com/geocode/json',
-  params: {latlng: '40.714224,-73.96145', language: 'en'},
-  headers: {
-    'X-RapidAPI-Key': '9d4b5f864amshaf0d8d49acd5c6ep15b0e3jsn19f524833588',
-    'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
 
 app.listen(3000,()=> {
     console.log('serving on port 3000')
